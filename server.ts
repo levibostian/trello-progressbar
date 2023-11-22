56b32839625e2d10871e5e9d5eec1055adc13194
Levi Bostian <levi@levi.earth>
2023-11-22 07:53:05 -0600

    host directory of public files, not just html

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
-import { serveFile } from "https://deno.land/std@0.207.0/http/file_server.ts";
+import { serveDir } from "https://deno.land/std@0.207.0/http/file_server.ts";
 
 Deno.serve((request: Request) => {
-  return serveFile(request, "./public/index.html");
+  return serveDir(request, {
+    fsRoot: "public"
+  })
 });
