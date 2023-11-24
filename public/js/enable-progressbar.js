10547bd5114ffe79f60f57647730e5dfa6897413
Levi Bostian <levi@levi.earth>
2023-11-24 07:55:07 -0600

    start

--- public/js/enable-progressbar.js
+++ public/js/enable-progressbar.js
@@ -1,7 +1,11 @@
 /* global TrelloPowerUp, moment, Pikaday */
 
 var Promise = TrelloPowerUp.Promise;
-var t = TrelloPowerUp.iframe();
+var t = TrelloPowerUp.iframe({
+  appKey: 'b6adec0a422f05429e2bfa1e2bed2b53',
+  appName: 'Progress Bar',
+  appAuthor: 'Curiosity IO'
+});
 var token = null;
 
 var TIME_FORMAT = 'LT';
@@ -15,57 +19,42 @@ let resize = function(){
   t.sizeTo('.container');
 };
 
-// t.get('card', 'shared', 'unixTime')
-// .then(function(unixTime) {
-//   if (unixTime) {
-//     // unhide remove button if card has a snooze time set
-//     document.getElementById('remove-btn').classList.remove('u-hidden');
-//     var existingMoment = new moment(unixTime * 1000);
-//     // set pikaday to match currently set snooze date
-//     picker.setMoment(existingMoment);
-//     document.getElementById('time-input').value = existingMoment.format(TIME_FORMAT);
-//   }
-// });
-
 document.getElementById('enable-btn').addEventListener('click', function(){
   console.log('enable-btn clicked');
 
   t.card('all')
-  .then(function(card){
-    console.log(JSON.stringify(card, null, 2));
+  .then(async function(card){
+    console.log(JSON.stringify(card, null, 2));    
 
-    // $.post('/snooze?', { token: token, cardId: card.id, snoozeTime: unixTime }, function(){
-    //   return t.set('card', 'shared', { idCard: card.id, time: snoozeTime, unixTime: unixTime })
-    //   .then(function(){
-    //     t.closePopup();
-    //   });
-    // });
+    if (card.due === null || card.start === null) {
+      // TODO: modify pop-up to indicate that you cannot enable progress bar on card until dates added. for now, we will just log an error for dev. 
+      console.error('Cannot enable progress bar on card without due and start dates');
+      return 
+    }
+
+    let cardTitle = card.name;
+    // "due": "2023-12-08T12:52:00.000Z",
+    // "start": "2023-11-24T14:00:00.000Z",
+    let dueDate = new Date(card.due);
+    let startDate = new Date(card.start);
+    let now = new Date();
+
+    let daysDoneThusFar = (now.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24); // TODO: wrong value 
+    let daysBetweenDueDateAndStartDate = (dueDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24); // TODO: wrong value 
+
+    let progressPercentage = (daysDoneThusFar / daysBetweenDueDateAndStartDate) * 100; // TODO: get whole number, not float 
+
+    title = `${cardTitle} - ${progressPercentage}% ${daysBetweenDueDateAndStartDate} days`;
+
+    console.log(`days done: ${daysDoneThusFar}, total days: ${daysBetweenDueDateAndStartDate}, progress percentage: ${progressPercentage}`)
+
+    // TODO: make sure title is correct. 
+    console.log(`new title: ${title}`)            
   })
   .catch(function(err){
     console.error(err);
   });
 });
-
-// document.getElementById('remove-btn').addEventListener('click', function(){
-//   t.card('id')
-//   .then(function(card){
-//     $.ajax({
-//       url: `/snooze/${card.id}?` + $.param({ token: token }),
-//       type: 'DELETE',
-//       success: function(){
-//         return t.set('card', 'shared', { idCard: null, time: null, unixTime: null })
-//         .then(function(){
-//           t.closePopup();
-//         });
-//       },
-//       error: function(err){ console.error('Error deleting from server: ' + JSON.stringify(err)); }
-//     });
-//   })
-//   .catch(function(err){
-//     console.error('Error unarchiving card');
-//     console.error(err);
-//   });
-// });
 
 t.render(function(){
   resize();
