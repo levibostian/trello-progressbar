75e7d665c3ba85b341993cc27f020cbc179d0423
Levi Bostian <levi@levi.earth>
2023-11-26 07:17:28 -0600

    fix: inject api key in response (#3)

--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
-import { serveDir } from "https://deno.land/std@0.207.0/http/file_server.ts";
+import { Eta } from "https://deno.land/x/eta@v3.1.0/src/index.ts";
+
+const eta = new Eta({ views: "./public" });
 
 Deno.serve((request: Request) => {
-  return serveDir(request, {
-    fsRoot: "public"
-  })
+  const requestUrl = new URL(request.url);
+  let path = requestUrl.pathname;
+  if (path == "/") path = "index";
+  path = path.replace(".html", "");
+
+  return new Response(eta.render(path, { TRELLO_API_KEY: Deno.env.get("TRELLO_API_KEY") }), { status: 200, headers: { "content-type": "text/html" } });
 });
